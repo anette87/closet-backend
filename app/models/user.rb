1d133f75497403621efa5fa2dc3a4f1d62fceee3
anette87 <anettem.rivera@gmail.com>
2020-09-28 16:53:53 -0700

    user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     belongs_to :closet
     has_many :items, through: :closet 
     validates :email, presence: true, uniqueness: true
-    validates :name, presence: true,
+    validates :name, presence: true
 end
