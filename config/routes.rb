6454890ec05db72ee5e0f4eee654da74c6a2bc2b
anette87 <anettem.rivera@gmail.com>
2020-10-07 19:33:38 -0700

    set app to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :closets
   resources :items
   resources :users
+
+  root 'closets#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
